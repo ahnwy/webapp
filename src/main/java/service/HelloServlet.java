bc32a48e3252c169aa1772b188928160a6f20d75
wonyoung <awy900@gmail.com>
2023-01-25 18:34:57 +0900

    Initial commit

---
+++ src/main/java/service/HelloServlet.java
@@ -0,0 +1,12 @@
+package service;
+
+import config.SimpleServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends SimpleServlet {
+
+}
